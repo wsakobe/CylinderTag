83156f440f877b80e90fed25b5b88300e97288b1
wsakobe <542110515@qq.com>
2022-11-18 21:29:43 +0800

    commit

--- CylinderTag.cpp
+++ CylinderTag.cpp
@@ -69,16 +69,33 @@ void CylinderTag::detect(const Mat& img, vector<MarkerInfo>& cornerList, int ada
     Mat imgMark(img.rows, img.cols, CV_32FC3);
     cvtColor(img, imgMark, COLOR_GRAY2RGB);
 
-    Mat img_resize;
-    resize(img, img_resize, Size(img.cols / 2, img.rows / 2));
+	//time record
+	clock_t start[10];  
+    double duration[10]; 
+	start[0] = clock();
+    
+	Mat img_resize;
+    resize(img, img_resize, Size(img.cols / 2, img.rows / 2), 0.5, 0.5, INTER_CUBIC);
     img_resize.convertTo(img_resize, CV_32FC1, 1.0 / 255);
+
+	start[1] = clock();
+	duration[0] = (double)(start[1] - start[0]) / CLOCKS_PER_SEC; 
 
     Mat img_binary(img_resize.rows, img_resize.cols, CV_8UC1);
     detector.adaptiveThreshold(img_resize, img_binary, adaptiveThresh);
-	// imshow("1", img_binary);
-	// waitKey(0);
-    detector.connectedComponentLabeling(img_binary, quadAreas_labeled);
-    detector.edgeExtraction(img_resize, quadAreas_labeled, corners);
+
+	start[2] = clock();
+	duration[1] = (double)(start[2] - start[1]) / CLOCKS_PER_SEC; 
+
+	detector.connectedComponentLabeling(img_binary, quadAreas_labeled);
+
+	start[3] = clock();
+	duration[2] = (double)(start[3] - start[2]) / CLOCKS_PER_SEC; 
+
+    detector.edgeExtraction(img_resize, quadAreas_labeled, corners, meanG);
+
+	start[4] = clock();
+	duration[3] = (double)(start[4] - start[3]) / CLOCKS_PER_SEC; 
 
     if (cornerSubPix){
     	detector.edgeSubPix(img, corners, corners, cornerSubPixDist);
@@ -88,7 +105,10 @@ void CylinderTag::detect(const Mat& img, vector<MarkerInfo>& cornerList, int ada
 			circle(imgMark, corners[i][j], 3, Scalar(75, 92, 196), -1);
 	}
 	
-    detector.featureRecovery(corners, features);
+    detector.featureRecovery(corners, features, meanG);
+
+	start[5] = clock();
+	duration[4] = (double)(start[5] - start[4]) / CLOCKS_PER_SEC; 
 
 	for (int i = 0; i < features.size(); i++){
         line(imgMark, features[i].corners[0], features[i].corners[1], Scalar(255, 241, 67), 1);
@@ -101,6 +121,7 @@ void CylinderTag::detect(const Mat& img, vector<MarkerInfo>& cornerList, int ada
         line(imgMark, features[i].corners[3], features[i].corners[0], Scalar(255, 241, 67), 1);
         circle(imgMark, features[i].feature_center, 2, Scalar(75, 92, 196));
 		putText(imgMark, to_string(i), features[i].corners[0], FONT_ITALIC, 1, Scalar(200, 200, 100), 1);
+		cout << features[i].firstDarker << endl;
     }
     
 	imshow("Feature Organization", imgMark);
@@ -109,6 +130,12 @@ void CylinderTag::detect(const Mat& img, vector<MarkerInfo>& cornerList, int ada
     detector.featureExtraction(img, features, features);
 	detector.markerOrganization(features, markers);
 	detector.markerDecoder(markers, markers, this->state);
+
+	start[6] = clock();
+	duration[5] = (double)(start[6] - start[5]) / CLOCKS_PER_SEC; 
+	cout << duration[0] * 1000 << " " << duration[1] * 1000 << " " << duration[2] * 1000 << " " << duration[3] * 1000 << " " << duration[4] * 1000 << " " << duration[5] * 1000 << " " << endl;
+	double ttime = duration[0] + duration[1] + duration[2] + duration[3] + duration[4] + duration[5];
+	cout << "Total time: " << ttime * 1000 << endl;
 }
 
 void CylinderTag::loadModel(const string& path, vector<ModelInfo> reconstruct_model){
